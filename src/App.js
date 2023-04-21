9094430b307e66caae58ebb9fdcd62781ccaa63d
Janaina Gabriel <estevao09.gabriel@gmail.com>
2023-04-21 20:08:06 -0300

    Mensagem de commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import './App.css';
 
 function App() {
+  const [dados, setDados] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/dados')
+      .then(response => {
+        setDados(response.data);
+  console.log(response.data)
+
+
+
+        
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  if (dados.length === 0) {
+    return <div>Carregando...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        {dados.map(item => (
+          <Col key={item.id} xs={12} md={6} lg={4}>
+            <Card className="mb-3">
+              <Card.Img variant="top" src={item.imagem} />
+              <Card.Body>
+                <Card.Title>{item.titulo}</Card.Title>
+                <Card.Text>{item.descricao}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
